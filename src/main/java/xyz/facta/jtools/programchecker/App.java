91c19db91c1271d5960bde4df80ac562e17f3cdd
Xiuheng <wuxhme@gmail.com>
2023-12-04 18:20:10 +0800

    Handle a dir of files and enable colorized output

--- src/main/java/xyz/facta/jtools/programchecker/App.java
+++ src/main/java/xyz/facta/jtools/programchecker/App.java
@@ -5,13 +5,19 @@ import com.github.javaparser.ParseResult;
 import com.github.javaparser.ast.CompilationUnit;
 
 import org.apache.commons.cli.*;
+
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
 
 public class App {
+    private static final String ANSI_RESET = "\u001B[0m";
+    private static final String ANSI_GREEN = "\u001B[32m";
+    private static final String ANSI_RED = "\u001B[31m";
     public static void main(String[] args) throws IOException {
-
 
         Options options = new Options();
 
@@ -20,32 +26,60 @@ public class App {
         options.addOption(input);
 
         CommandLineParser parser = new DefaultParser();
-        HelpFormatter formatter = new HelpFormatter();
         CommandLine cmd;
-
         try {
             cmd = parser.parse(options, args);
+            String inputPath = cmd.getOptionValue("input");
+
+            File inputFile = new File(inputPath);
+
+            if (inputFile.isFile()) {
+                printFilePath(inputFile);
+                tryParseFile(inputFile);
+            } else if (inputFile.isDirectory()) {
+                List<File> javaFiles = listJavaFiles(inputFile);
+                for (File javaFile : javaFiles) {
+                    printFilePath(javaFile);
+                    tryParseFile(javaFile);
+                }
+            } else {
+                System.err.println("Invalid input path. Please specify a valid Java file or directory.");
+            }
         } catch (ParseException e) {
-            System.out.println(e.getMessage());
+            System.err.println("Error parsing command-line options: " + e.getMessage());
+            HelpFormatter formatter = new HelpFormatter();
             formatter.printHelp("ProgramChecker", options);
             System.exit(1);
             return;
         }
-
-
-        String inputFilePath = cmd.getOptionValue("input");
-        Path filePath = Paths.get(inputFilePath);
-        tryParseFile(filePath);
     }
-
-    private static void tryParseFile(Path path) throws IOException {
+    private static void printFilePath(File file) {
+        System.out.println(file.getAbsolutePath());
+    }
+    private static void tryParseFile(File file) throws IOException {
         JavaParser javaParser = new JavaParser();
-        ParseResult<CompilationUnit> result = javaParser.parse(path);
+        ParseResult<CompilationUnit> result = javaParser.parse(file);
 
         if (result.isSuccessful() && result.getResult().isPresent()) {
-            System.out.println("OK");
+            System.out.println(ANSI_GREEN + "OK" + ANSI_RESET);
         } else {
-            result.getProblems().forEach(problem -> System.out.println(problem.getMessage()));
+            result.getProblems().forEach(problem -> System.out.println(ANSI_RED + problem.getMessage() + ANSI_RESET));
         }
+    }
+
+
+    private static List<File> listJavaFiles(File directory) {
+        List<File> javaFiles = new ArrayList<>();
+        File[] files = directory.listFiles();
+
+        if (files != null) {
+            for (File file : files) {
+                if (file.isFile() && file.getName().endsWith(".java")) {
+                    javaFiles.add(file);
+                }
+            }
+        }
+
+        return javaFiles;
     }
 }
